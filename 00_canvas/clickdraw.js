2cb7dbe4150cf91cf9442ef6877eb29e3b6b6b82
sxia1 <sxia1@stuy.edu>
2019-01-30 18:31:30 -0500

    cw

---
+++ 00_canvas/clickdraw.js
@@ -0,0 +1,24 @@
+var c = document.getElementById("slate");
+var ctx = c.getContext("2d");
+, 50, 100, 200);
+});
+
+//upon button push clear the canvas
+var clearB = document.getElementById("clear");
+clearB.addEventListener('click', function(){
+
+});
+
+//upon button push toggle
+var toggB = document.getElementById("toggle");
+toggB.addEventListener('click', function(){
+
+});
+
+/*
+fillStyle
+strokeStyle
+clearRect()
+fillText
+ellipse()
+*/
