8f5dc2cd340c4af55206a8797f2137f130226425
Sophia Xia <sxia1@stuy.edu>
2019-01-30 19:37:08 -0500

    completed

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,24 +1,37 @@
+/*
+  Sophia Xia
+  SoftDev2 pd8
+  K00 -- I See a Red Door...
+  2018-01-30
+*/
+
+var mode = 1;
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
-, 50, 100, 200);
+ctx.fillStyle = 'green';
+ctx.strokeStyle = 'blue';
+
+//Clicking in the canvas draws a rectangle or ellipse
+c.addEventListener('click', function(e){
+    console.log(e);
+    if(mode > 0){
+	ctx.fillRect(e.offsetX, e.offsetY, 10, 20);
+    }
+    else{
+	ctx.beginPath();
+	ctx.ellipse(e.offsetX, e.offsetY, 20, 20, 0, 0, 2*Math.PI);
+	ctx.stroke();
+    }
 });
 
 //upon button push clear the canvas
 var clearB = document.getElementById("clear");
 clearB.addEventListener('click', function(){
-
+    ctx.clearRect(0, 0, 600, 600);
 });
 
 //upon button push toggle
 var toggB = document.getElementById("toggle");
 toggB.addEventListener('click', function(){
-
+    mode = mode *-1;
 });
-
-/*
-fillStyle
-strokeStyle
-clearRect()
-fillText
-ellipse()
-*/
