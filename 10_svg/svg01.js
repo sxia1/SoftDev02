4f6a7b76ccaf2e7e04f0a1e224fd80b18b032362
Sophia Xia <sxia1@stuy.edu>
2019-03-14 07:40:39 -0400

    attempt

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -1,20 +1,52 @@
 var pic = document.getElementById("vimage")
 
 pic.addEventListener('click', function(e){
+    draw(e);
+});
+
+var draw = function(e){
     var x = e.offsetX;
     var y = e.offsetY;
     console.log(x, y);
-    //CIRCLE
+    circle(x, y);
+    console.log(e.clientX - e.offsetX);
+    console.log(e.clientY - e.offsetY);
+}
+
+var circle = function(x, y){
     var c = document.createElementNS(
-    "http://www.w3.org/2000/svg", "circle");
+	"http://www.w3.org/2000/svg", "circle");
     c.setAttribute("cx", x);
     c.setAttribute("cy", y);
     c.setAttribute("r", 10);
     c.setAttribute("fill", "blue");
     pic.appendChild(c);
-});
+    c.addEventListener('click', function(e){
+	change_color(e, c);
+    });
+    c.addEventListener('click', function(e){
+	move_random(e, c);
+    });
+}
 
+var change_color = function(e, c){
+    if(c.getAttribute("fill") == "blue"){
+	c.setAttribute("fill", "green");
+    }
+    else if(c.getAttribute("fill") == "green"){
+	pic.removeChild(c);
+	var circ = circle(100, 100);
+	pic.appendChild(circ);
+    }
+}
 
+var move_random = function(e, c){
+    if(c.getAttribute("fill") == "green"){
+	pic.removeChild(c);
+	//var circ = circle(100, 100);
+	//pic.appendChild(circ);
+    }
+}
 
 //CLEAR BUTTON
 var clearB = document.getElementById("but_clear");
@@ -22,6 +54,4 @@ clearB.addEventListener('click', function(e){
     while(pic.lastChild){
 	pic.removeChild(pic.lastChild);
     }
-    prevX = -1;
-    prevY = -1;
 });
